99478103867a78c71716a096b929ac10e9ebc725
LakelChester <lakel@chesterassociate.com>
2022-05-14 14:23:54 -0700

    commit tree 422b7122522fe01577d998c5fbfb289a8da41516

---
+++ index.js
@@ -0,0 +1,51 @@
+function shout(string){
+    return string.toUpperCase()
+}
+
+function whisper(string){
+    return string.toLowerCase()
+}
+
+function logShout(string){
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+    //const string = 'Hello!'
+    //const lowerCaseResponse 
+    //let responseToRoomate
+
+    if(string = string.toLowerCase()){
+        'I can\'t hear you!' 
+    } else if(string === console.log(string.toUpperCase())){
+        'YES INDEED!'
+    } else if(string === console.log(string.tolowerCase())){
+        "I can't hear you!"
+    }
+
+    return 'I would love to!' 
+}   
+ // sayHiToHeadphonedRoommate('')
+    
+    //string === "Let's have dinner together!"
+    //return "I would love to!"
+
+    
+    // string === console.log(string.tolowerCase()):
+    //responseToRoomate = "I can't hear you!"
+       
+    //string = console.log(string.toUpperCase()):
+    //responseToRoomate = "YES INDEED!"
+    
+    
+    //const string = "Hello!";
+
+    //console.log(string);
+    //console.log(string.toUpperCase());
+    
+    //console.log(string === string.toUpperCase());
